167d6df5a122b0e0e1bbe2fee268f9b49d359a5f
bmrogers2022 <bmrogers@ucsd.edu>
2024-01-03 16:08:41 -0800

    Add sample odds
    add this past season's odds to work on tests with

---
+++ NFL_code/AutoDraft/FantasySimdraft.py
@@ -0,0 +1,8 @@
+"""
+Because our program is reliant on data of mock drafts for unique league settings which
+we will not have access to, we will have to simulate our own drafts and check which strategies
+work the best inside of our simulations. This will mostly determine the relative values of positions,
+not individual players since player positions will be made easily with their vegas odds
+"""
+
+# TODO: Make a draft simulator to collect data
